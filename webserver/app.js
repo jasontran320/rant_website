a363a2bae03394092983c13c1ffe4df63d57cb38
jasontran320 <jasont16@uci.edu>
2025-09-07 09:33:50 -0700

    Update app.js

--- webserver/app.js
+++ webserver/app.js
@@ -8,7 +8,7 @@ const express = require('express');
 const connectDB = require('./server/config/db');
 
 const app = express();
-const PORT = 5000 || process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 //connect to db
 connectDB();
